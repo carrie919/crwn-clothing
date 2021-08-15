6b7f1025524c6b64157cbf2414e54bc1ddefc75b
carrie919 <harshasunandh919@gmail.com>
2021-08-15 21:40:24 +0530

    using reselect module replace cart selectors MEMOIZED selectors so component wouldn't re render everytime

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import {toggleCart} from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCart }) => (
+const CartIcon = ({ toggleCart, itemCount }) => (
     <div className='cart-icon' onClick={() => toggleCart()}>
         <ShoppingIcon className='shopping-bag' />
-        <span className='item-count'>0</span> 
+        <span className='item-count'>{itemCount}</span> 
     </div>
 );
 
 const mapDispatchToProps = dispatch => ({
     toggleCart: () => dispatch(toggleCart())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
